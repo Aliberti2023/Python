029e74a8ef9a00f031ac7fec69734402bc139c0d
Tiago Aliberti <tiago_aliberti@hotmail.com>
2023-09-27 08:33:05 -0300

    Calcular IMC

---
+++ Calcular IMC.py
@@ -0,0 +1,12 @@
+nome = 'Teste'
+altura = 1.80
+peso = '78'
+imc = 74 / (1.77 * 1.77)
+
+linha_1  = f'{nome} tem  {altura:.2f} de altura'
+linha_2 = f' pesa {peso} quilos e seu imc é'
+linha_3 = f'{imc:.2f}'
+
+print(linha_1)
+print(linha_2)
+print(linha_3)
